60077e91bf46dad46239a2d1f3487d40e2a56600
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-07-27 15:33:44 +0600

    user billing api

--- user/user.js
+++ user/user.js
@@ -6,9 +6,24 @@ const user = (app, db) => {
         res.send(result)
     })
 
-    app.get(`/user/:email`, async (req, res) => {
+    app.get(`/a/:email`, async (req, res) => {
         const email = req.params.email
         const result = await db.findOne({ email });
+        res.send(result)
+    })
+    app.put(`/user/bill/:email`, async (req, res) => {
+        const email = req.params.email
+        const info = req.body
+        const options = { upsert: true };
+        const updateDoc = {
+            $set: {
+                bill: info
+            },
+        };
+        const filter = await db.findOne({ email });
+        const result = await db.updateOne(filter, updateDoc, options);
+        console.log(result)
+
         res.send(result)
     })
 
