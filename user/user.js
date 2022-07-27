76605239cd12b7ca95f8865dca0a5bbb47d55167
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-07-27 16:56:48 +0600

    shipping info api

--- user/user.js
+++ user/user.js
@@ -6,7 +6,10 @@ const user = (app, db) => {
         res.send(result)
     })
 
-    app.get(`/a/:email`, async (req, res) => {
+
+
+
+    app.get(`/user/bill/:email`, async (req, res) => {
         const email = req.params.email
         const result = await db.findOne({ email });
         res.send(result)
@@ -14,16 +17,26 @@ const user = (app, db) => {
     app.put(`/user/bill/:email`, async (req, res) => {
         const email = req.params.email
         const info = req.body
-        const options = { upsert: true };
-        const updateDoc = {
-            $set: {
-                bill: info
-            },
-        };
+        const updateDoc = { $set: { bill: info } };
         const filter = await db.findOne({ email });
-        const result = await db.updateOne(filter, updateDoc, options);
-        console.log(result)
+        const result = await db.updateOne(filter, updateDoc, { upsert: true });
+        res.send(result)
+    })
 
+
+
+
+    app.get(`/user/shipping/:email`, async (req, res) => {
+        const email = req.params.email
+        const result = await db.findOne({ email });
+        res.send(result)
+    })
+    app.put(`/user/shipping/:email`, async (req, res) => {
+        const email = req.params.email
+        const info = req.body
+        const updateDoc = { $set: { shipping: info } };
+        const filter = await db.findOne({ email });
+        const result = await db.updateOne(filter, updateDoc, { upsert: true });
         res.send(result)
     })
 
