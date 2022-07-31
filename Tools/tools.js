338bbd3bffe14870b9ec7cccd25b3c363e98e6e2
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-08-01 02:44:18 +0600

    delete tool functon api end point

--- Tools/tools.js
+++ Tools/tools.js
@@ -1,7 +1,18 @@
+const {  ObjectId } = require('mongodb');
+
 const tools = (app, db) => {
 
     app.get(`/tools`, async (req, res) => {
         const result = await db.find().toArray()
+        res.send(result)
+    })
+
+    app.delete(`/tool-delete`, async (req, res) => {
+        const _id = req.body._id
+        console.log(_id)
+        
+        const result = await db.deleteOne({ _id: ObjectId(_id) } );
+
         res.send(result)
     })
 
