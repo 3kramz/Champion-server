97bf3b5acd4512d2565f47259527613f47263ca4
Sultan Mahmud Ekram <smahmud1098@gmail.com>
2022-08-01 01:20:56 +0600

    update order status

--- Order/order.js
+++ Order/order.js
@@ -1,6 +1,11 @@
 const {  ObjectId } = require('mongodb');
 const order = (app, db) => {
 
+    app.get(`/order`, async (req, res) => {
+      const result = await db.find().toArray()
+      res.send(result)
+    })
+
     app.get(`/order/:email`, async (req, res) => {
       const email = req.params.email;
       const result = await db.find({ email }).toArray()
@@ -18,6 +23,23 @@ const order = (app, db) => {
     app.get(`/order-status/:id`, async (req, res) => {
       const _id = req.params.id;
       const result = await db.findOne({ _id: ObjectId(_id) })
+      res.send(result)
+    })
+
+
+    app.get(`/order-status/:id`, async (req, res) => {
+      const _id = req.params.id;
+      const result = await db.findOne({ _id: ObjectId(_id) })
+      res.send(result)
+    })
+
+    app.put(`/order-status/:id`, async (req, res) => {
+      const id = req.params.id;
+      const status = req.body.status;
+   
+      const updateDoc = { $set: { status: status } };
+      const result = await db.updateOne({ _id: ObjectId(id) }, updateDoc, { upsert: true });
+      
       res.send(result)
     })
 
